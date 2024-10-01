55e50227fa10cfd85e7fe043801bec7f5d37e9ef
Josh Kautz <josh@joshkautz.com>
2024-10-01 17:22:49 -0500

    Added displacement map to a Sphere, and to a model with unknown geometry. Also added image texture mapping.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,44 @@
 
 import React from "react";
 import { Canvas, ThreeElements } from "@react-three/fiber";
-import { useGLTF, OrbitControls } from "@react-three/drei";
+import { useGLTF, OrbitControls, useTexture } from "@react-three/drei";
+import * as THREE from "three";
+
+const Sphere = (props: ThreeElements["mesh"]) => {
+  const displacementMap = useTexture("displacementMap.jpg");
+  const map = useTexture("clown.jpg");
+
+  const geometry = new THREE.SphereGeometry(1, 64, 64);
+  const material = new THREE.MeshStandardMaterial({
+    color: "darkgreen",
+    map: map,
+    displacementMap: displacementMap,
+    displacementScale: 0.25,
+  });
+
+  return <mesh geometry={geometry} material={material} {...props} />;
+};
 
 const Model = (props: ThreeElements["group"]) => {
   const { scene } = useGLTF("clay.glb");
+  const displacementMap = useTexture("displacementMap.jpg");
+  const map = useTexture("clown.jpg");
+
+  scene.traverse((child) => {
+    if (child instanceof THREE.Mesh) {
+      child.material.map = map;
+      child.material.displacementMap = displacementMap;
+      child.material.displacementScale = 0.2; // Adjust the scale as needed
+      child.material.needsUpdate = true;
+    }
+  });
+
   return <primitive object={scene} {...props} />;
 };
 
 useGLTF.preload("clay.glb");
+useTexture.preload("displacementMap.jpg");
+useTexture.preload("clown.jpg");
 
 export default function Home() {
   return (
@@ -23,7 +53,8 @@ export default function Home() {
         intensity={Math.PI}
       />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Model position={[0, 0, 0]} scale={[2, 2, 2]} />
+      <Sphere position={[-1.5, 0, 0]} scale={[1, 1, 1]} />
+      <Model position={[1.5, 0, 0]} scale={[1, 1, 1]} />
       <OrbitControls />
     </Canvas>
   );
