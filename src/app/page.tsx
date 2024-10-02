847213f9bc9a042291bd434b95777b5e07ce4ebe
Josh Kautz <josh@joshkautz.com>
2024-10-02 13:34:02 -0500

    Successfully added random programatic positional displacement for vertices on a Sphere geometry.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,23 +9,50 @@ import {
   StatsGl,
   useTexture,
 } from "@react-three/drei";
-// import * as THREE from "three";
+import * as THREE from "three";
+
+const addSphereGeometryDisplacement = (geometry: THREE.SphereGeometry) => {
+  console.log(geometry.attributes.position.count);
+  for (let i = 0; i < geometry.attributes.uv.array.length / 2; i++) {
+    // u is stored at even indices (v is stored at odd indices)
+    const u = geometry.attributes.uv.array[i * 2];
+
+    // Vertices where u = 0 or u = 1 are on the seam
+    if (u !== 0 && u !== 1) {
+      // Vertices at the top of the sphere.
+      if (i >= geometry.parameters.widthSegments) {
+        // Vertices at the bottom of the sphere.
+        if (
+          i <
+          geometry.attributes.position.count - geometry.parameters.widthSegments
+        ) {
+          const index = i * 3;
+          geometry.attributes.position.array[index] += Math.random() * 0.025; // x coordinate of the vertex
+          geometry.attributes.position.array[index + 1] +=
+            Math.random() * 0.025; // y coordinate
+          geometry.attributes.position.array[index + 2] +=
+            Math.random() * 0.025; // z coordinate
+          console.log(i);
+        }
+      }
+    }
+  }
+};
 
 const Sphere = () => {
   const normalMap = useTexture("RaZum_Clay_Nm15.png");
-  const displacementMap = useTexture("RaZum_Clay_Dm15.png");
+
+  const sphereGeometry = new THREE.SphereGeometry(1, 64, 64);
+  addSphereGeometryDisplacement(sphereGeometry);
 
   return (
-    <mesh position={[0, 0, 0]} scale={[3, 3, 3]}>
-      <sphereGeometry args={[1, 64, 64]} />
+    <mesh position={[0, 0, 0]} scale={[3, 3, 3]} geometry={sphereGeometry}>
       <meshStandardMaterial
+        // wireframe={true}
         color="#C29F8A"
         metalness={0.0}
         roughness={0.9}
         normalMap={normalMap}
-        // normalScale={[0.5, 0.5]}
-        displacementMap={displacementMap}
-        displacementScale={0.2}
       />
     </mesh>
   );
