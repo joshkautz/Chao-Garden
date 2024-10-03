ccde978eb09296e241f1888f5f63f1f45689d259
Josh Kautz <josh@joshkautz.com>
2024-10-02 20:07:40 -0500

    Getting comfortable working with objects/groups that have multiple meshes. I've found that it's best to not mege the meshes. I know that it's possible, and fairly easy. But unless I have a clear and obvious reason to do that, I'm going to avoid it. UV Mapping proves to still be tricky. I'm going to push that down the road.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ import { MTLLoader } from "three/addons/loaders/MTLLoader.js";
 import { TextureLoader } from "three/src/loaders/TextureLoader.js";
 import { LoopSubdivision } from "three-subdivide";
 import * as THREE from "three";
+import * as BufferGeometryUtils from "three/examples/jsm/utils/BufferGeometryUtils.js";
 
 const applySubdivisionModifier = (
   geometry: THREE.BufferGeometry<THREE.NormalBufferAttributes>
@@ -99,35 +100,38 @@ const addClayMaterial = (
 const Model = () => {
   const texture = useLoader(
     TextureLoader,
-    "test/imperfection_0002_color_2k.jpg"
+    "Textures/imperfection_0002_color_2k.jpg"
   );
-  const mtl = useLoader(MTLLoader, "Chao Egg.mtl");
-  const obj = useLoader(OBJLoader, "Chao Egg.obj", (loader) => {
+  const mtl = useLoader(MTLLoader, "Tails/Tails.mtl");
+  const obj = useLoader(OBJLoader, "Tails/Tails.obj", (loader) => {
     mtl.preload();
     loader.setMaterials(mtl);
   });
 
-  let geometry = (
-    obj.children.find((child) => child instanceof THREE.Mesh) as THREE.Mesh
-  ).geometry;
-
-  let material = (
-    obj.children.find((child) => child instanceof THREE.Mesh) as THREE.Mesh
-  ).material;
-
-  geometry = addGeometryDisplacement(geometry);
-
-  geometry = applySubdivisionModifier(geometry);
-
-  material = addClayMaterial(material, texture);
+  obj.traverse((child) => {
+    if (child instanceof THREE.Mesh) {
+      // child.geometry = addGeometryDisplacement(child.geometry);
+      child.geometry = applySubdivisionModifier(child.geometry);
+      child.material.bumpMap = texture;
+      child.material.bumpScale = 10;
+    }
+  });
 
   return (
-    <mesh
-      geometry={geometry}
-      material={material}
-      position={[0, 0, 0]}
-      scale={[1, 1, 1]}
-    />
+    <>
+      {/* <primitive object={obj} position={[0, 0, 0]} scale={[1, 1, 1]} /> */}
+      <group position={[0, 0, 0]}>
+        {obj.children
+          .filter((child) => child instanceof THREE.Mesh)
+          .map((child, index) => (
+            <mesh
+              key={index}
+              geometry={child.geometry}
+              material={child.material}
+            />
+          ))}
+      </group>
+    </>
   );
 };
 
