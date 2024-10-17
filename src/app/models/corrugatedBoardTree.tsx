62679eb516518d4c78fddea4a7e379b4df41791a
Josh Kautz <josh@joshkautz.com>
2024-10-17 14:08:39 -0500

    Just committing this right now because I'm able to do Subtractions and Intersections with my own extruded 3d geometries from SVGs  with basic shapes.

--- src/app/models/corrugatedBoardTree.tsx
+++ src/app/models/corrugatedBoardTree.tsx
@@ -1,29 +1,29 @@
 "use client";
 
-import { GroupProps, useLoader } from "@react-three/fiber";
-import { SVGLoader } from "three/examples/jsm/Addons.js";
-import React, { useMemo } from "react";
-
+import { GroupProps } from "@react-three/fiber";
+import React, { forwardRef, useEffect, useMemo, useRef, useState } from "react";
 import {
-  Box3,
-  DoubleSide,
-  Euler,
-  ExtrudeGeometry,
   Mesh,
+  DoubleSide,
   Vector2,
   Vector3,
+  BufferGeometry,
+  Euler,
+  Group,
 } from "three";
 
 export interface CorrugatedBoardTreeProps extends GroupProps {
   location?: Vector2;
+  thickness?: number;
 }
 
 export const CorrugatedBoardTree = ({
   location,
   scale,
   rotation,
+  thickness,
 }: CorrugatedBoardTreeProps) => {
-  const SCALE = 0.05;
+  const SCALE = 1;
   const SCALE_X = scale instanceof Vector3 ? scale.x : 1;
   const SCALE_Y = scale instanceof Vector3 ? scale.y : 1;
   const SCALE_Z = scale instanceof Vector3 ? scale.z : 1;
@@ -41,43 +41,56 @@ export const CorrugatedBoardTree = ({
     if (locationX < 0) x = locationX + 0.5;
     if (locationY > 0) y = locationY - 0.5;
     if (locationY < 0) y = locationY + 0.5;
-    return new Vector3(x, 0, y);
-  }, [locationX, locationY]);
+    return new Vector3(x, SCALE_Y / 2, y);
+  }, [locationX, locationY, SCALE_Y]);
 
-  const { paths } = useLoader(SVGLoader, "trees/tree_01.svg");
+  const WAVES = 4;
+  const THICKNESS = thickness ?? 0.1;
+  const meshRef = useRef<Mesh>(null);
 
-  const geometry = useMemo(() => {
-    const tempGeometry = new ExtrudeGeometry(
-      paths.map((path) => SVGLoader.createShapes(path)).flat(),
-      {
-        depth: 20, // Depth of extrusion
+  useEffect(() => {
+    if (meshRef.current) {
+      const geometry = meshRef.current.geometry as BufferGeometry;
+      const positionAttribute = geometry.attributes.position;
+      const positions = positionAttribute.array as Float32Array;
+
+      // Modify vertices to create a wave effect
+      const waveFrequency = WAVES * (Math.PI * 2) * SCALE_Y; // Controls the frequency of the waves
+      const waveAmplitude = THICKNESS + 0.01; // Controls the height of the waves. Add a small extra to clip and add texture.
+
+      for (let i = 0; i < positions.length; i += 3) {
+        positions[i + 2] =
+          Math.sin(positions[i + 1] * waveFrequency + Math.PI / 2) *
+          waveAmplitude;
       }
-    );
 
-    // Rotate the geometry 180 degrees around the X-axis and center it
-    tempGeometry.rotateX(Math.PI);
-    tempGeometry.center();
-
-    // Compute the bounding box
-    const boundingBox = new Box3().setFromObject(new Mesh(tempGeometry));
-    const yOffset = boundingBox.min.y;
-
-    // Adjust the position to align the bottom of the mesh at Y = 0
-    tempGeometry.translate(0, -yOffset, 0);
-
-    return tempGeometry;
-  }, [paths]);
+      // Mark the positions attribute as needing an update
+      positionAttribute.needsUpdate = true;
+    }
+  }, [SCALE_Y, THICKNESS]);
 
   return (
-    <mesh
+    <group
       position={POSITION}
       scale={[SCALE_X * SCALE, SCALE_Y * SCALE, SCALE_Z * SCALE]}
       rotation={ROTATION}
-      geometry={geometry}
-      castShadow={true}
-      receiveShadow={true}
     >
-      <meshStandardMaterial color="#9C8D7B" side={DoubleSide} />
-    </mesh>
+      <mesh position={[0, 0, THICKNESS]} castShadow={true} receiveShadow={true}>
+        <planeGeometry />
+        <meshStandardMaterial color="#9C8D7B" side={DoubleSide} />
+      </mesh>
+      <mesh ref={meshRef} castShadow={true} receiveShadow={true}>
+        <planeGeometry args={[1, 1, 1, SCALE_Y * (WAVES * 2)]} />
+        <meshStandardMaterial color="#9C8D7B" side={DoubleSide} />
+      </mesh>
+      <mesh
+        position={[0, 0, -THICKNESS]}
+        castShadow={true}
+        receiveShadow={true}
+      >
+        <planeGeometry />
+        <meshStandardMaterial color="#9C8D7B" side={DoubleSide} />
+      </mesh>
+    </group>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,54 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { PresentationControls, Stats, StatsGl } from "@react-three/drei";
 
 import { CorrugatedBoard } from "./models/corrugatedBoard";
-import { Euler, Vector2, Vector3 } from "three";
-import { CorrugatedBoardTree } from "./models/corrugatedBoardTree";
+import {
+  BufferGeometry,
+  DoubleSide,
+  Euler,
+  ExtrudeGeometry,
+  Group,
+  Mesh,
+  Vector2,
+  Vector3,
+} from "three";
+import { ExtrudedTree, extrudedTreeGeometry } from "./models/extrudedTree";
 import { calculateCameraPosition } from "./utilities/calculateCameraPosition";
 
+import {
+  Addition,
+  Base,
+  Geometry,
+  Intersection,
+  Subtraction,
+} from "@react-three/csg";
+
+import { CSG } from "three-csg-ts";
+
 export default function Home() {
-  console.log(calculateCameraPosition());
+  const [geometry, setGeometry] = useState<ExtrudeGeometry>();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error>();
+
+  useEffect(() => {
+    const loadGeometry = async () => {
+      try {
+        const extrudedTree = await extrudedTreeGeometry();
+        setGeometry(extrudedTree);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadGeometry();
+  }, []);
 
   return (
     <Canvas camera={{ position: [0, 0, 10], fov: 75 }} shadows={true}>
@@ -26,12 +64,45 @@ export default function Home() {
           rotation={new Euler(0, Math.PI / 4, 0)}
           thickness={0.1}
         />
-
-        <CorrugatedBoardTree
+        <ExtrudedTree
           location={new Vector2(2, 2)}
           scale={new Vector3(1, 1, 1)}
           rotation={new Euler(0, Math.PI / 4, 0)}
         />
+
+        <mesh position={[4, 0, 2]} castShadow receiveShadow>
+          <Geometry useGroups>
+            <Base geometry={geometry} castShadow receiveShadow>
+              <meshStandardMaterial color="#9C8D7B" side={DoubleSide} />
+            </Base>
+            <Intersection
+              scale={[2, 1, 1]}
+              position={[1, 1, 1]}
+              castShadow
+              receiveShadow
+            >
+              <sphereGeometry args={[1, 32, 32]} />
+              <meshStandardMaterial color="#9C8D7B" side={DoubleSide} />
+            </Intersection>
+          </Geometry>
+        </mesh>
+
+        <mesh position={[4, 0, 1.5]} castShadow receiveShadow>
+          <Geometry useGroups>
+            <Base geometry={geometry} castShadow receiveShadow>
+              <meshStandardMaterial color="#9C8D7B" side={DoubleSide} />
+            </Base>
+            <Subtraction
+              scale={[2, 1, 1]}
+              position={[1, 1, 1]}
+              castShadow
+              receiveShadow
+            >
+              <sphereGeometry args={[1, 32, 32]} />
+              <meshStandardMaterial color="#9C8D7B" side={DoubleSide} />
+            </Subtraction>
+          </Geometry>
+        </mesh>
 
         <gridHelper args={[30, 30]} />
         <axesHelper args={[10]} />
